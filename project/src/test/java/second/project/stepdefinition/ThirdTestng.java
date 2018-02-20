d13115a34f09cd9b9c1584110ecfe7ea14ce022e
sowmya <sowmya@192.168.100.8>
2018-02-20 20:20:40 +0530

    this changes are done the the personal laptop

---
+++ project/src/test/java/second/project/stepdefinition/ThirdTestng.java
@@ -0,0 +1,10 @@
+package second.project.stepdefinition;
+
+import org.testng.annotations.Test;
+
+public class ThirdTestng {
+	@Test
+	public void test1() {
+		System.out.println("I have created thi class in my personal laptop");
+	}
+}
