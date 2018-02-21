d7b4f6fba61811ffe7bee9c1804c089aab332d96
Sowmya_Murukutla <Sowmya_Murukutla@EPINHYDW0706.princeton.epam.com>
2018-02-21 10:42:23 +0530

    changed the sop statement

--- project/src/test/java/second/project/stepdefinition/FirstJunit.java
+++ project/src/test/java/second/project/stepdefinition/FirstJunit.java
@@ -5,20 +5,20 @@ import org.testng.annotations.Test;
 public class FirstJunit {
 	@Test
 	public void test1() {
-		System.out.println("This is first test menthod");
+		System.out.println("This is first test method");
 	}
 	
 	@Test
 	public void test2() {
-		System.out.println("This is second test menthod");
+		System.out.println("This is second test method");
 	}
 	@Test
 	public void test3() {
-		System.out.println("This is third test menthod");
+		System.out.println("This is third test method");
 	}
 	@Test
 	public void test4() {
-		System.out.println("This is fourth test menthod");
+		System.out.println("This is fourth test method");
 	}
 	
 }
--- project/src/test/java/second/project/stepdefinition/SecondJunit.java
+++ project/src/test/java/second/project/stepdefinition/SecondJunit.java
@@ -5,19 +5,19 @@ import org.testng.annotations.Test;
 public class SecondJunit {
 	@Test
 	public void test1() {
-		System.out.println("This is SECOND CLASS first test menthod");
+		System.out.println("This is SECOND CLASS first test method");
 	}
 	
 	@Test
 	public void test2() {
-		System.out.println("This is SECOND CLASS second test menthod");
+		System.out.println("This is SECOND CLASS second test method");
 	}
 	@Test
 	public void test3() {
-		System.out.println("This is SECOND CLASS third test menthod");
+		System.out.println("This is SECOND CLASS third test method");
 	}
 	@Test
 	public void test4() {
-		System.out.println("This is SECOND CLASS fourth test menthod");
+		System.out.println("This is SECOND CLASS fourth test method");
 	}
 }
